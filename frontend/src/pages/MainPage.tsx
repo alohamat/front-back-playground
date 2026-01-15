b0644c83113e610969d85ba6f53de7534aabcf31
aloha <matalohazk@gmail.com>
2026-01-15 09:05:12 -0300

    feat: animated background

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -1,7 +1,9 @@
 export default function MainPage() {
   return (
-    <div>
-      <h1>Hello, world!</h1>
+    <div id="all">
+      <main className="h-screen bg-linear-to-br from-indigo-800 via-purple-900 to-blue-950 animated-bg">
+        <h1 className="text-white text-2xl">Hello, world!</h1>
+      </main>
     </div>
   );
 }
